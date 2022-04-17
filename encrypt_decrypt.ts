4f897426ac7f8230c80dbacbc9fa98b99e8c357e
Vibhavi Peiris <vibi9505@gmail.com>
2022-04-17 16:14:59 -0400

    Aes working, fixed bigint size issue

--- encrypt_decrypt.ts
+++ encrypt_decrypt.ts
@@ -4,10 +4,10 @@ let { encodeInto } = require('bigint-serialiser');
 
 export function encrypt(key,plaintext){
   let textBytes = AES.utils.utf8.toBytes(plaintext);
-  let keyBytes = new Uint8Array(32); // [ 0, 0, 0, 0, 0 ]
+  let keyBytes = new Uint8Array(100); // [ 0, 0, 0, 0, 0 ]
   encodeInto(key, keyBytes); // 4
 
-  let aesCtr = new AES.ModeOfOperation.ctr(keyBytes, new AES.Counter(5));
+  let aesCtr = new AES.ModeOfOperation.ctr(keyBytes.slice(0, 32), new AES.Counter(5));
 
   let encryptedBytes = aesCtr.encrypt(textBytes);
   let encryptedHex = AES.utils.hex.fromBytes(encryptedBytes);
@@ -17,11 +17,10 @@ export function encrypt(key,plaintext){
 
 export function decyrpt(key,ciphertext){
   var ciphertextBytes = AES.utils.hex.toBytes(ciphertext);
-  let keyBytes = new Uint8Array(32); // [ 0, 0, 0, 0, 0 ]
+  let keyBytes = new Uint8Array(100); // [ 0, 0, 0, 0, 0 ]
   encodeInto(key, keyBytes); // 4
 
-
-  let aesCtr = new AES.ModeOfOperation.ctr(keyBytes, new AES.Counter(5));
+  let aesCtr = new AES.ModeOfOperation.ctr(keyBytes.slice(0, 32), new AES.Counter(5));
   var decryptedBytes = aesCtr.decrypt(ciphertextBytes);
 
   // Convert our bytes back into text
