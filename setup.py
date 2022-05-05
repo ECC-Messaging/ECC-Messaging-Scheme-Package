111968f90188511b9398582c01fc645fecfac263
Vibhavi Peiris <vibi9505@gmail.com>
2022-05-05 19:20:06 -0400

    Added readme and pushed new version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="ecc-messaging-scheme-package",
-    version="1.0.1",
+    version="1.0.2",
     description="ECCDH AES encryption/decryption Package",
     long_description=README,
     long_description_content_type="text/markdown",
